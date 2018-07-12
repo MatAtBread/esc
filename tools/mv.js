20e5b5764690be36effa8e6f21d7d620831775d7
matthew.woolf@mailonline.co.uk <matthew.woolf@mailonline.co.uk>
2018-07-12 11:46:20 +0100

    Add `mv`, copes with src==dest

---
+++ tools/mv.js
@@ -0,0 +1,23 @@
+/* ES cp - copy src to dest */
+
+module.exports = async function(es,args,config,flags) {
+	if (args.length < 2) throw module.exports.help ;
+
+	if (args[0]===args[1]) {
+		// Move to myself is a special case - we do the copy/reindex since we've been asked to
+		// Do a copy
+		var dest = args[1]+"-"+process.pid ;
+		await require('./cp')(es,[args[0],dest],config,flags) ;
+		// Then delete the src
+		await require('./rm')(es,[args[0]],config,flags) ;
+		await require('./cp')(es,[dest,args[0]],config,flags) ;
+		await require('./rm')(es,[dest],config,flags) ;
+	} else {
+		// Do a copy
+		await require('./cp')(es,args,config,flags) ;
+		// Then delete the src
+		await require('./rm')(es,[args[0]],config,flags) ;
+	}
+} ;
+
+module.exports.help = "move <src> index to <dest> index. Use --nomappings to move docs only (not mappings)".magenta ;
