39e852d70653584e6f12ea155b1636177fa6a378
matthew.woolf@mailonline.co.uk <matthew.woolf@mailonline.co.uk>
2018-07-12 11:44:27 +0100

    Change --mappings to --nomappings. Removed failed copies

--- tools/cp.js
+++ tools/cp.js
@@ -4,7 +4,7 @@ module.exports = async function(es,args,config,flags) {
 	if (args.length < 2) throw module.exports.help ;
 
 	var mappings ;
-	if (flags.mappings)
+	if (!flags.nomappings)
 		mappings = await es.indices.getMapping({ index: args[0] }) ;
 	
 	await es.indices.create({
@@ -15,17 +15,28 @@ module.exports = async function(es,args,config,flags) {
 		}
 	}) ;
 
-	var d = await es.reindex({
-		refresh: true,
-		body:{
-			source: {
-				index: args[0]
-			},
-			dest: {
-				index: args[1]
-			}		
-		}
-	});
+	try {
+		await es.reindex({
+			refresh: true,
+			body:{
+				source: {
+					index: args[0]
+				},
+				dest: {
+					index: args[1]
+				}		
+			}
+		});
+	} catch(ex) {
+		if (ex.body) {
+			if (ex.body.created !== ex.body.total) {
+				d = await es.indices.delete({
+					index:args[1]
+				});
+				throw ("reindex "+args[0]+" into "+args[1]+" failed ("+ex.body.created+" of "+ex.body.total+" copied). Did you set --nomappings (not mappings)?").red ;
+			}
+		} else throw ex ;
+	}
 } ;
 
-module.exports.help = "copy <src> index to <dest> index. Use --mappings to copy mappings as well".magenta ;
+module.exports.help = "copy <src> index to <dest> index. Use --nomappings to copy docs only".magenta ;
