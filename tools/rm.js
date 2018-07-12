928a27fd051c9499c54dffeef02df3705d5ed171
matthew.woolf@mailonline.co.uk <matthew.woolf@mailonline.co.uk>
2018-07-12 11:45:18 +0100

    rm: remove aliases first, then indices

--- tools/rm.js
+++ tools/rm.js
@@ -3,20 +3,23 @@
 module.exports = async function(es,args,config,flags) {
 	if (!args.length) throw module.exports.help ;
 	var d ;
-	try {
-		d = await es.indices.deleteAlias({
-			name:args[0],
-			index:'_all'
-		});
-	} catch(ex) {
-		if (ex.statusCode === 404) {
-			d = await es.indices.delete({
-				index:args[0]
+	for (var i=0; i<args.length; i++) {
+		var idx = args[i] ;
+		try {
+			d = await es.indices.deleteAlias({
+				name:idx,
+				index:'_all'
 			});
-		} else {
-			throw ex ;
+		} catch(ex) {
+			if (ex.statusCode === 404) {
+				d = await es.indices.delete({
+					index:idx
+				});
+			} else {
+				throw ex ;
+			}
 		}
 	}
 }
 
-module.exports.help = "delete <index, alias or pattern>.".magenta+" WARNING: cannot be reversed".red ;
+module.exports.help = "delete <index, alias or pattern...>.".magenta+" WARNING: cannot be reversed".red ;
