20540252ed8223f232da6a5c85242ac48e55fcb2
matthew.woolf@mailonline.co.uk <matthew.woolf@mailonline.co.uk>
2018-07-17 08:42:17 +0100

    cp: only call "create" if mappings are being copied (allows for merge) cp: don't delete on failure (allows for merge) cp: leave "task" in .tasks index cp: '--query={ json }' to restrict source data

--- tools/cp.js
+++ tools/cp.js
@@ -13,23 +13,24 @@ module.exports = async function(es,args,config,flags) {
 	var src = m ? m[2]:args[0] ;
 
 	var mappings ;
-	if (!flags.nomappings)
+	if (!flags.nomappings) {
 		mappings = await esSrc.indices.getMapping({ index: src }) ;
+		await es.indices.create({
+			index: args[1],
+			body:{
+				mappings:mappings ? mappings[src].mappings:undefined,
+				settings: config['settings.index']
+			}
+		}) ;
+	}
 	
-	await es.indices.create({
-		index: args[1],
-		body:{
-			mappings:mappings ? mappings[src].mappings:undefined,
-			settings: config['settings.index']
-		}
-	}) ;
-
 	var body = {
 		wait_for_completion: false,
 		refresh: true,
 		body:{
 			source: {
-				index: src
+				index: src,
+				query: flags.query ? JSON.parse(flags.query) : undefined
 			},
 			dest: {
 				index: args[1]
@@ -44,6 +45,8 @@ module.exports = async function(es,args,config,flags) {
 		var task, reindex = await es.reindex(body);
 		do {
 			task = await es.tasks.get({ taskId: reindex.task }) ;
+			if (task.error)
+				throw {body: task.task.status, message: task.error.type+" "+task.error.reason+"\t"+task.task.description} ;
 			var num = ((task.task.status.created / task.task.status.total) * 1000|0)/10+"%" ;
 			num = num+"      ".slice(0,6-num.length) ;
 			process.stdout.write(" "+num+"\x1B[7D") ;
@@ -51,13 +54,7 @@ module.exports = async function(es,args,config,flags) {
 		}  while(!task.completed) ;
 		process.stdout.write("       \x1B[7D") ;
 	} catch(ex) {
-		try {
-			d = await es.indices.delete({
-				index:args[1]
-			});
-		} catch (ex) {
-			console.warn("Possibly truncated index "+args[1]+" created!") ;
-		}
+		console.warn("Possibly truncated index "+args[1]+" created!") ;
 		if (ex.body) {
 			if (!ex.body.created)
 				throw ex ;
